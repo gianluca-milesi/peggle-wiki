8c7360fa32e784fb4a636af4241cc1b585c79f60
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-02-08 17:28:08 +0100

    Starting express server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,14 @@
-console.log("ciao");
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+
+app.get("/", (req, res) => {
+    res.send("Root")
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server in ascolto sulla porta ${PORT}`)
+})
