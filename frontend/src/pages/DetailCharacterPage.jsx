be6b8a244d63fccd2bb3a470ddb90b19ae7b0e16
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-02-11 00:50:33 +0100

    ADD: fetchCharacterData fetching character and reviews

--- frontend/src/pages/DetailCharacterPage.jsx
+++ frontend/src/pages/DetailCharacterPage.jsx
@@ -1,43 +1,63 @@
 import axios from "axios"
-//Contexts
-import GlobalContext from "../contexts/GlobalContext.js"
 //Hooks
-import { useContext, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 //Components
+import BackButton from "../components/BackButton.jsx"
 import DetailCharacter from "../components/DetailCharacter.jsx"
 import CardReview from "../components/CardReview.jsx"
 
 
 function DetailCharacterPage() {
 
+    const [character, setCharacter] = useState([])
+    const [reviews, setReviews] = useState([])
+    const { id } = useParams()
+
+    function fetchCharacterData() {
+        Promise.all([
+            axios.get(`http://localhost:3000/api/characters/${id}`),
+            axios.get(`http://localhost:3000/api/characters/${id}/reviews`)
+        ])
+            .then(([characterRes, reviewsRes]) => {
+                setCharacter(characterRes.data)
+                setReviews(reviewsRes.data)
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    }
+
+    useEffect(() => {
+        fetchCharacterData()
+    }, [id])
+
 
     return (
         <>
-            <section>
+            <section className="my-5">
+                <div className="mb-5 px-5"><BackButton /></div>
                 <div className="container">
-                    {characters &&
-                        <DetailCharacter item={characters} />
+                    {character &&
+                        <DetailCharacter item={character} />
                     }
                 </div>
             </section>
 
-            <section>
-                <div className="reviews-headline flex justify-between">
+            <section className="my-5">
+                <div className="reviews-headline flex justify-between px-5">
                     <h2>Recensioni</h2>
-                    <p>{characters.avgVote}</p>
+                    <p>{character.avgVote}</p>
                 </div>
                 <div className="container">
                     <div className="row">
-                        {review.length > 0 ? (
-                            review.map((data) => (
+                        {reviews &&
+                            reviews.map((data) => (
                                 <div className="col-6" key={data.id}>
                                     <CardReview item={data} />
                                 </div>
                             ))
-                        ) : (
-                            <p>Nessuna recensione disponibile.</p>
-                        )}
+                        }
                     </div>
                 </div>
             </section>
